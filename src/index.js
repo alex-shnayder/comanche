5217019621b0c16ad251cc8e70dce3464873f869
Alex Shnayder <alex@shnayder.pro>
2017-05-14 14:12:05 +0700

    CLI plugin outline

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const comanche = require('./comanche')
 const api = require('./plugins/api')
+const cli = require('./plugins/cli')
 const run = require('./plugins/run')
 
-const DEFAULT_PLUGINS = [api, run]
+const DEFAULT_PLUGINS = [api, cli, run]
 
 module.exports = function defaultComanche(...args) {
   let plugins = DEFAULT_PLUGINS
