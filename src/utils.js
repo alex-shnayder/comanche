e06d3f0762513aa8968847a1c29628e36c055a27
Alex Shnayder <alex@shnayder.pro>
2017-05-21 21:30:44 +0700

    Simplify findMatch

--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,12 @@
-module.exports.findMatch = (haystack, needle) => {
-  if (!Array.isArray(haystack)) {
-    throw new Error('A haystack must be an array of commands or options')
-  }
-
-  let isString = (typeof needle === 'string')
-  let isObject = (typeof needle === 'object')
-
-  if (!needle || (!isString && !isObject)) {
-    throw new Error('A needle must be either a string or an object')
-  }
-
-  if (isString) {
-    return haystack.find((item) => {
-      let { name, alias } = item
-      return (name === needle || (alias && alias.includes(needle)))
-    })
-  }
-
+function findMatch(items, needle) {
   let names = needle.alias ? needle.alias.concat(needle.name) : [needle.name]
 
-  return haystack.find((item) => {
+  return items.find((item) => {
     let { name, alias } = item
-    let itemNames = alias ? alias.concat(name) : [name]
-    return names.find((n) => itemNames.includes(n))
+    return names.find((n) => n === name || (alias && alias.includes(n)))
   })
+}
+
+module.exports = {
+  findMatch,
 }
