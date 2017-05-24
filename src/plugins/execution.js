8875e2c11332a7edb55758268e51c04bd58a18e4
Alex Shnayder <alex@shnayder.pro>
2017-05-25 02:03:21 +0700

    Add basic validation of commands in dispatch

--- src/plugins/execution.js
+++ src/plugins/execution.js
@@ -6,6 +6,34 @@ module.exports = function executionPlugin(lifecycle) {
       throw new Error('The first argument of dispatch must be an array of commands')
     }
 
+    commands = commands.map((command) => {
+      if (!command || typeof command !== 'object') {
+        throw new Error('A command must be an object')
+      }
+
+      let { name, options } = command
+
+      if (typeof name === 'string') {
+        if (!name.length) {
+          throw new Error('A command name must not be empty')
+        }
+
+        name = [name]
+      }
+
+      if (!Array.isArray(name)) {
+        throw new Error('A command name must be an array or a string')
+      }
+
+      options = options || {}
+
+      if (!options || typeof options !== 'object') {
+        throw new Error('Options must be an object')
+      }
+
+      return { name, options }
+    })
+
     return yield next(commands)
   })
 
