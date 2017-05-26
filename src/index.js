90277330e74db5a821763d6947dc077bc886f5fa
Alex Shnayder <alex@shnayder.pro>
2017-05-26 17:59:45 +0700

    Canonize plugin

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ const api = require('./plugins/api')
 const cli = require('./plugins/cli')
 const execution = require('./plugins/execution')
 const validation = require('./plugins/validation')
+const canonize = require('./plugins/canonize')
 const camelCase = require('./plugins/camelCase')
 
-const DEFAULT_PLUGINS = [execution, api, cli, validation, camelCase]
+const DEFAULT_PLUGINS = [execution, api, cli, validation, canonize, camelCase]
 
 module.exports = function defaultComanche(...args) {
   let plugins = DEFAULT_PLUGINS
---
+++ src/plugins/canonize.js
@@ -0,0 +1,19 @@
+const { next } = require('hooter/effects')
+
+module.exports = function canonizePlugin(lifecycle) {
+  lifecycle.hookAfter('execute', function* (commands) {
+    commands = commands.map((command) => {
+      if (command.options) {
+        command = Object.assign({}, command)
+        command.options = command.options.map((option) => {
+          let outputName = option.config ? option.config.name : option.name
+          return Object.assign({}, option, { outputName })
+        })
+      }
+
+      return command
+    })
+
+    return yield next(commands)
+  })
+}
