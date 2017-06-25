156a6be7e6d4054abe3f5af9931833d1333aedab
Alex Shnayder <alex@shnayder.pro>
2017-06-25 19:02:08 +0700

    Tests for camelize plugin

---
+++ src/plugins/camelize/test.js
@@ -0,0 +1,75 @@
+const assert = require('assert')
+const { processHandler, camelizeOptions } = require('./index?private')
+const camelizePlugin = require('./index')
+
+
+describe('camelize plugin', () => {
+  it('hooks the process handler', () => {
+    let generator = camelizePlugin()
+    let { value } = generator.next()
+
+    assert.deepStrictEqual(value, {
+      effect: 'hook',
+      event: 'process',
+      mode: 'end',
+      handler: processHandler,
+    })
+  })
+
+  describe('process handler', () => {
+    let config = {}
+    let command = {}
+    let generator = processHandler(config, command)
+
+    it('calls camelizeOptions', () => {
+      let { value } = generator.next()
+
+      assert.deepStrictEqual(value, {
+        effect: 'call',
+        fn: camelizeOptions,
+        args: [command],
+      })
+    })
+
+    it('yields "next" with the camelized command', () => {
+      let camelizedCommand = {}
+      let { value } = generator.next(camelizedCommand)
+
+      assert.deepStrictEqual(value, {
+        effect: 'next',
+        args: [config, camelizedCommand],
+      })
+    })
+
+    it('returns the result', () => {
+      let result = {}
+      let { value, done } = generator.next(result)
+
+      assert(value === result)
+      assert(done)
+    })
+  })
+
+  describe('camelizeOptions', () => {
+    it('returns the given command if it has no options', () => {
+      let command = {}
+      let result = camelizeOptions(command)
+      assert(command === result)
+    })
+
+    it('converts the command\'s options to camelCase', () => {
+      let command = {
+        options: [{
+          outputName: 'foo-bar',
+        }],
+      }
+      let result = camelizeOptions(command)
+
+      assert.deepStrictEqual(result, {
+        options: [{
+          outputName: 'fooBar',
+        }],
+      })
+    })
+  })
+})
