e725c9cd3df163549efda41a957a18e8ef3dba44
Alex Shnayder <alex@shnayder.pro>
2017-06-25 21:14:14 +0700

    Rework canonize plugin so that it doesn't set output names when no config

--- src/plugins/canonize/index.js
+++ src/plugins/canonize/index.js
@@ -4,17 +4,21 @@ const { next, hookEnd, call } = require('hooter/effects')
 function canonize(command) {
   let { fullName, config, options } = command
 
-  if (!config) {
-    return command
+  if (config) {
+    command = Object.assign({}, command)
+    command.outputName = fullName.slice(0, -1).concat(config.name)
   }
 
-  command = Object.assign({}, command)
-  command.outputName = fullName.slice(0, -1).concat(config.name)
-
   if (options && options.length) {
+    command = config ? command : Object.assign({}, command)
     command.options = options.map((option) => {
-      let outputName = option.config ? option.config.name : option.name
-      return Object.assign({}, option, { outputName })
+      if (!option.config) {
+        return option
+      }
+
+      return Object.assign({}, option, {
+        outputName: option.config.name,
+      })
     })
   }
 
