a063cae15ee4538976c88c5007cc66f3a0832f1c
Alex Shnayder <alex@shnayder.pro>
2017-10-04 08:23:07 +0700

    Update appache

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 /* eslint-disable global-require */
 
 const appache = require('appache')
-const pluginize = require('appache/pluginize')
-
-
-const defaultPlugins = {
-  api: require('appache-api-fluent'),
-  cli: require('appache-cli'),
-  prettyErrors: require('appache-pretty-errors'),
-}
+const defaultPlugins = [
+  require('appache-api-fluent'),
+  require('appache-cli'),
+  require('appache-pretty-errors'),
+]
 
 
 module.exports = function comanche(...args) {
-  let userPlugins = (Array.isArray(args[args.length - 1])) ? args.pop() : null
-  let plugins = pluginize(null, defaultPlugins, userPlugins)
-  let app = appache(plugins)
-  return app.apply(null, args)
+  let plugins = (Array.isArray(args[args.length - 1])) ? args.pop() : null
+  plugins = plugins ? defaultPlugins.concat(plugins) : defaultPlugins
+  let createCommand = appache(plugins)
+  let app = createCommand(...args)
+  app.start = createCommand.start
+  app.catch = createCommand.catch
+  return app
 }
